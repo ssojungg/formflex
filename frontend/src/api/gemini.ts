bd8eee77b1804fef6d63c9838ccd49e576a50767
ssojungg <kawy.sojung@gmail.com>
2026-05-07 23:55:03 +0900

    gemini.js 의 vite_url 수정

--- frontend/src/api/gemini.ts
+++ frontend/src/api/gemini.ts
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_URL;
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const generateChoicesAPI = async (prompt: string): Promise<string[]> => {
   const res = await fetch(`${BASE_URL}/surveys/gemini/choices`, {
