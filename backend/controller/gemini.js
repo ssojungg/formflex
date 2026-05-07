2c1d3d162c97a967ab0066864d2a15542fc982bc
ssojungg <kawy.sojung@gmail.com>
2026-05-08 01:57:44 +0900

    gemini 버전 수정

--- backend/controller/gemini.js
+++ backend/controller/gemini.js
@@ -1,7 +1,7 @@
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: 'gemini-2.0-flash' });
+const model = genAI.getGenerativeModel({ model: 'gemini-2.5-flash-preview-04-17' });
 
 // 선택지 생성
 const generateChoices = async (req, res) => {
